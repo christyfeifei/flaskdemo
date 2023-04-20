d2e033abfa8a9a73eb3f4407d1063c5d7e63a939
christy <vennesach@gmail.com>
2023-04-20 14:56:51 +0800

    輸出pm2.5表格

--- pm25.py
+++ pm25.py
@@ -8,9 +8,9 @@ def get_pm25():
         url = 'https://data.epa.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON'
         resp = requests.get(url)
         datas = resp.json()['records']
-        # 組合標題
+        # 組合標題 <一維>
         columns = list(datas[0].keys())
-        # 組合內容
+        # 組合內容 <二維>
         values = []
         for data in datas:
             values.append(list(data.values()))
