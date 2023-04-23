99f8cfe898a5d469f78ecc645ff5ec5db90e4d7d
christy <vennesach@gmail.com>
2023-04-23 20:25:32 +0800

    完成排序功能(使用GET&POST)

--- pm25.py
+++ pm25.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def get_pm25():
+def get_pm25(sort=False):
     columns, values = None, None
     try:
         url = 'https://data.epa.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON'
@@ -13,7 +13,14 @@ def get_pm25():
         # 組合內容 <二維>
         values = []
         for data in datas:
-            values.append(list(data.values()))
+            data = list(data.values())
+            try:
+                data[2] = eval(data[2])
+                values.append(data)
+            except Exception as e:
+                print(e)
+        if sort:
+            values = sorted(values, key=lambda x: x[2], reverse=True)
 
     except Exception as e:
         print(e)
